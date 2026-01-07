8a7b83695ea8f040be2c6f76bf16a674d14e1059
sam <samuelonasis23@gmail.com>
2026-01-07 09:34:12 +0700

    chore: feat some image

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,7 +69,23 @@ export const metadata: Metadata = {
     },
   },
   icons: {
-    icon: "/assets/logo_transparent.png",
+    icon: [
+      { url: "/assets/favicon.ico" },
+      { url: "/assets/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/assets/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      {
+        url: "/assets/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/assets/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+    shortcut: "/assets/favicon.ico",
+    apple: [{ url: "/assets/apple-touch-icon.png" }],
   },
 };
 
