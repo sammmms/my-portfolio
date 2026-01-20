70ff424f0e232c9145b74c0c30e4326b39c831fa
sam <samuelonasis23@gmail.com>
2026-01-20 15:13:10 +0700

    chore: change sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,29 +2,30 @@ import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://wsnsam.my.id";
+  const today = new Date().toISOString().split("T")[0];
 
   return [
     {
       url: baseUrl,
-      lastModified: new Date(),
+      lastModified: today,
       changeFrequency: "yearly",
       priority: 1,
     },
     {
       url: `${baseUrl}/projects`,
-      lastModified: new Date(),
+      lastModified: today,
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
       url: `${baseUrl}/experience`,
-      lastModified: new Date(),
+      lastModified: today,
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
       url: `${baseUrl}/contact`,
-      lastModified: new Date(),
+      lastModified: today,
       changeFrequency: "yearly",
       priority: 0.5,
     },
